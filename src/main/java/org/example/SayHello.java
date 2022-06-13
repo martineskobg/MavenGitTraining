23bd6d9645922c88231297ffa7e7706c023afba1
Martin <m.bachev86@gmail.com>
2022-06-13 11:18:56 +0300

    new test methods in TestSayHello.java

--- src/main/java/org/example/SayHello.java
+++ src/main/java/org/example/SayHello.java
@@ -2,7 +2,24 @@ package org.example;
 
 public class SayHello {
 
-    public String sayHello(){
+    private String message;
+
+    public SayHello(String message) {
+        this.message = message;
+    }
+
+    public String printMessage() {
+        System.out.println(message);
         return "Hello World";
     }
+
+    public void prntMessage() {
+        System.out.println(message);
+        int divide = 10 / 0;
+    }
+
+    public static boolean isOdd(int number) {
+        return number % 2 != 0;
+    }
+
 }
---
+++ src/test/java/org/example/TestSayHello.java
@@ -0,0 +1,63 @@
+package org.example;
+
+import org.junit.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import javax.print.attribute.TextSyntax;
+import java.time.Month;
+
+import static org.junit.Assert.assertTrue;
+
+public class TestSayHello {
+    String message = "Hello World";
+    SayHello sayHello = new SayHello(message);
+
+//    @BeforeClass
+//    public static void beforeClass() {
+//        System.out.println("Before Class");
+//    }
+//
+//    @AfterClass
+//    public static void afterClass() {
+//        System.out.println("After Class");
+//    }
+//
+//    @Before
+//    public void beforeMethod() {
+//        System.out.println("Before Method");
+//    }
+//
+//    @After
+//    public void afterMethod() {
+//        System.out.println("After Method");
+//    }
+
+    @Test
+    public void testHelloWorld() {
+        Assert.assertEquals(message, sayHello.printMessage());
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void testPrntMessage() {
+        System.out.println("Martin Is Printing");
+        sayHello.prntMessage();
+    }
+
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 3, 5, -3, 15, Integer.MAX_VALUE})
+    public void isOddTest(int number) {
+        assertTrue(sayHello.isOdd(number));
+    }
+
+    @ParameterizedTest
+    @EnumSource(Month.class)
+    public void getValForMonth(Month month) {
+        int id = month.getValue();
+        System.out.println(month.name());
+        assertTrue(id >= 1 && id <= 12);
+    }
+
+}
